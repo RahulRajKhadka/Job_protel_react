d87330a2bcd7ba18c3c5e2f347def841b9cb6eed
RAHUL RAJ KHADKA <sirrahul9848@gmail.com>
2025-12-23 11:29:53 +0545

    first commit

---
+++ .history/src/App_20251223112834.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-700'>Hello Nepal welcome </div>
+    </>
+  )
+}
+
+export default App
